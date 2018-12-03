4e28998f21e23c899f2d8f27bc36c79156538cc9
Shamix <nsammi11@gmail.com>
2018-12-03 01:46:03 -0500

    Task: Model created and added 3 properties

---
+++ database/migrations/2018_12_03_063647_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('make');
+            $table->text('model');
+            $table->text('year');
+            $table->Integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
