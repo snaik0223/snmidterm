2ea355cc4c85515884c4f3d59dcf677112af89dc
Shamix <nsammi11@gmail.com>
2018-12-03 19:03:46 -0500

    Task: Unit Test- InsertCarTest

---
+++ tests/Unit/InsertCarTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class InsertCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertCar()
+    {
+        $car = new Car();
+        $car->Make = 'Lexus';
+        $car->Model = '450h';
+        $car->year = '2019';
+        $this->assertTrue($car->save());
+    }
+}
